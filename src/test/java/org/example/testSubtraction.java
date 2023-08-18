013dfae1ddf1d60a4540d1edea032cee883156b2
Lukew3531 <luke_r_wakefield@hotmail.com>
2023-08-18 12:57:26 +0200

    Sub Test2

--- src/test/java/org/example/testSubtraction.java
+++ src/test/java/org/example/testSubtraction.java
@@ -1,5 +1,17 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class testSubtraction {
-    assertEquals(2, c.subtract(4,2))
+    @Test
+    void testAddition() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4,2));
+    }
 }
+
+
+
+
